0556217341e7f9e825a8d74a7c278ee023e6d988
flintyhosting <flinty@rimortech.co.uk>
2022-04-26 20:38:55 +0100

    hug.js

---
+++ commands/fun/hug.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'hug',
+    category: 'fun',
+    run: async (client, message, args) => {
+        message.delete({timeout: 0})
+
+        
+    }
+}
