60936aa48905e77e9efd8c0a1171cf7b423ad9a3
flintyhosting <flinty@rimortech.co.uk>
2022-04-27 07:00:21 +0100

    hug

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -6,6 +6,10 @@ module.exports = {
     run: async (client, message, args) => {
         message.delete({timeout: 0})
 
-        
+        const target = message.guild.members.cache.get(args[0]) || message.mentions.members.first()
+
+        const ranTargetedResponse = [`<@${message.member.id}> gave <@
+        ${target.user.id}> the biggest hug ever.`, `<@${target.user.id}> was given a great big squeeze by <@${message.member.id}>`]
+        const ranSelfReponse = [``]
     }
 }
